6e8bf974897dad1f8476a8ca68cb8b0df9806ef3
makin <max4nn@gmail.com>
2024-08-06 14:42:10 +0400

    попытка починить дейтпикер 2

--- src/App.vue
+++ src/App.vue
@@ -244,8 +244,6 @@ body {
   justify-content: center;
 }
 input[type='date'] {
-  -webkit-appearance: textfield;
-  -moz-appearance: textfield;
-  min-height: 1.2em; 
+  -webkit-appearance: none;
 }
 </style>
