6d853e5c97cf2d3fd26d73c5a6a6bd6702cbdfb2
makin <max4nn@gmail.com>
2024-08-08 15:47:18 +0400

    глобальный лоадинг

--- src/App.vue
+++ src/App.vue
@@ -1,62 +1,68 @@
 <template>
   <div id="app">
-    <div class="image-container">
-      <div class="character-container" :class="animationClass" >
-        <img v-if="headPiece" class="head-piece pixel-img" alt="head-piece" :src="headPiece">
-        <img class="character pixel-img" alt="marina" :src="bodyPiece">
-      </div>
-    </div>
-    <template v-if="section === 'info'">
-      <div>
-        <h2>Марина</h2>
-        <div class="info">
-          <h3>Уровень: {{ currentLevel }}</h3>
-          <h3>Золото: {{ gold }}</h3>
+    <template v-if="!initialLoading">
+      <div class="image-container">
+        <div class="character-container" :class="animationClass" >
+          <img v-if="headPiece" class="head-piece pixel-img" alt="head-piece" :src="headPiece">
+          <img class="character pixel-img" alt="marina" :src="bodyPiece">
         </div>
       </div>
-      <div class="progress-container pixel-border">
-        <div class="progress" :style="{ width: formattedProgress}"></div>
-      </div>
-      <button class="button pixel-border" @click="questsVisible = true">Квесты</button>
-      <button class="button pixel-border" @click="section = 'skills'">Навыки</button>
-      <button class="button pixel-border" @click="skinBookVisible = true">Шмотки</button>
+      <template v-if="section === 'info'">
+        <div>
+          <h2>Марина</h2>
+          <div class="info">
+            <h3>Уровень: {{ currentLevel }}</h3>
+            <h3>Золото: {{ gold }}</h3>
+          </div>
+        </div>
+        <div class="progress-container pixel-border">
+          <div class="progress" :style="{ width: formattedProgress}"></div>
+        </div>
+        <button class="button pixel-border" @click="questsVisible = true">Квесты</button>
+        <button class="button pixel-border" @click="section = 'skills'">Навыки</button>
+        <button class="button pixel-border" @click="skillBookVisible = true">Шмотки</button>
+      </template>
+      <template v-else>
+        <h2>Навыки</h2>
+        <h3>Скиллпоинты: {{ skillPoints }}</h3>
+        <button
+          v-for="skill in learnedSkills"
+          class="button pixel-border _compact"
+          :key="skill.id"
+          @click="playAnimation(skill.animation)"
+        >
+          {{ skill.shortName }}
+        </button>
+        <button class="button pixel-border _compact" @click="skillBookVisible = true">+</button>
+        <button class="button pixel-border" @click="section = 'info'">Назад</button>
+      </template>
+    
+      <quests
+        v-show="questsVisible" 
+        @close="closeQuests"
+        @add-exp="experienceBuffer += $event"
+        @add-gold="gold += $event"
+      />
+      <skill-book
+        v-show="skillBookVisible" 
+        :skill-points="skillPoints"
+        :learned-skill-ids="skillIds"
+        @close="closeSkillBook"
+        @learn-skill="learnSkill"
+      />
+      <skin-book 
+        v-show="skinBookVisible" 
+        :gold="gold" 
+        :equipment="equipment" 
+        :bought-items="boughtItems"
+        @close="skinBookVisible = false"
+        @update-info="initInfo"
+      />
     </template>
-    <template v-else>
-      <h2>Навыки</h2>
-      <h3>Скиллпоинты: {{ skillPoints }}</h3>
-      <button
-        v-for="skill in learnedSkills"
-        class="button pixel-border _compact"
-        :key="skill.id"
-        @click="playAnimation(skill.animation)"
-      >
-        {{ skill.shortName }}
-      </button>
-      <button class="button pixel-border _compact" @click="skillBookVisible = true">+</button>
-      <button class="button pixel-border" @click="section = 'info'">Назад</button>
-    </template>
-   
-    <quests
-      v-show="questsVisible" 
-      @close="closeQuests"
-      @add-exp="experienceBuffer += $event"
-      @add-gold="gold += $event"
-    />
-    <skill-book
-      v-show="skillBookVisible" 
-      :skill-points="skillPoints"
-      :learned-skill-ids="skillIds"
-      @close="closeSkillBook"
-      @learn-skill="learnSkill"
-    />
-    <skin-book 
-      v-show="skinBookVisible" 
-      :gold="gold" 
-      :equipment="equipment" 
-      :bought-items="boughtItems"
-      @close="skinBookVisible = false"
-      @update-info="initInfo"
-    />
+    <div v-else class="loader">
+      <img src="./assets/h.png" alt="loading" class="pixel-img head">
+      <h1>Загрузка...</h1>
+    </div>
   </div>
 </template>
 
@@ -91,11 +97,13 @@ export default {
       head: null,
       weapon: null,
      },
-     boughtItems: []
+     boughtItems: [],
+     initialLoading: true
     }
   },
   async created() {
-    this.initInfo()
+    await this.initInfo()
+    this.initialLoading = false
   },
   computed: {
     currentLevel() {
@@ -295,5 +303,24 @@ input[type='date'] {
   top: -49px;
   left: -10px;
 }
+@keyframes spin {
+  0% { transform: rotate(0deg); }
+  100% { transform: rotate(-360deg); }
+}
+.loader {
+  height: 70vh;
+  width: 100%;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  flex-direction: column;
+}
+
+.head {
+  animation-name: spin;
+  animation-duration: 1s;
+  animation-iteration-count: infinite;
+  animation-timing-function: linear;
+  width: 100px;
+}
 </style>
-./utils/utils
